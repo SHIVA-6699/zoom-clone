a97a4c0fca49b31e7c6ba2aeceb274b22151aa72
SHIVA-6699 <pillashivakrishna6@gmail.com>
2024-03-10 19:44:37 +0530

    Home Page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { ClerkProvider } from "@clerk/nextjs";
+import Navbar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -11,12 +12,23 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  
+
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
+
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar/>
+        <main className="max-w-5xl mx-auto p-5">
+
+        {children}
+        </main>
+        </body>
     </html>
+    </ClerkProvider>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import { SignedIn, UserButton, SignInButton, SignedOut } from "@clerk/nextjs";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <header className="shadow">
+      <div className="mx-auto flex h-14 max-w-5xl items-center justify-between p-3 font-medium">
+        <Link href={"/"}>New Meeting</Link>
+        <SignedIn>
+          <div className="flex items-center gap-4">
+            <Link href="/meetings">Meetings</Link>
+            <UserButton />
+          </div>
+        </SignedIn>
+        <SignedOut>
+            <SignInButton/>
+        </SignedOut>
+      </div>
+    </header>
+  );
+};
+export default Navbar;
